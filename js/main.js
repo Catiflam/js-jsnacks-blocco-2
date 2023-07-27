ff99c15f2a6efcd7846f487562c9d54d195cb49b
Catiflam <gilfrid1@gmail.com>
2023-07-27 14:19:58 +0200

    edit second snake

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-const firstNumber = parseInt(prompt("inserisci un numero"));
+// const firstNumber = parseInt(prompt("inserisci un numero"));
 
-if (firstNumber % 2 == 0) {
-	console.log("numero" + firstNumber);
-} else {
-	console.log("numero" + (firstNumber + 1));
+// if (firstNumber % 2 == 0) {
+// 	console.log("numero" + firstNumber);
+// } else {
+// 	console.log("numero" + (firstNumber + 1));
+// }
+
+const nomi = ["Marco", "Fabio", "Wender", "Paolo"];
+const cognomi = ["Mazzoli", "Alisei", "Giannatempo", "Noise"];
+
+const fintalista = [];
+
+const numeroFintaLista = prompt("Quanti invitati vuoi inserire");
+
+for (let i = 0; i < numeroFintaLista; i++) {
+	const randomNomiUser = MathF.loor(Math.random * nomi.length);
+	const randomNomi = nomi[randomNomiUser];
+
+	const randomCognomiUser = Math.floor(Math.random * cognomi.length);
+	const randomCognomi = cognomi[randomCognomiUser];
 }
