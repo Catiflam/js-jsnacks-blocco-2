2c357f30787229d719ba5508261b9a6f6ce9ec5f
Catiflam <gilfrid1@gmail.com>
2023-07-27 15:05:33 +0200

    edit snake 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// 1 SNAKE
+
 // const firstNumber = parseInt(prompt("inserisci un numero"));
 
 // if (firstNumber % 2 == 0) {
@@ -6,23 +8,43 @@
 // 	console.log("numero" + (firstNumber + 1));
 // }
 
-const nomi = ["Marco", "Fabio", "Wender", "Paolo"];
-const cognomi = ["Mazzoli", "Alisei", "Giannatempo", "Noise"];
+// 2 SNAKE
 
-const fintalistaInvitati = [];
+// const nomi = ["Marco", "Fabio", "Wender", "Paolo"];
+// const cognomi = ["Mazzoli", "Alisei", "Giannatempo", "Noise"];
 
-const numeroFintaLista = parseInt(prompt("Quanti  finti invitati vuoi inserire"));
+// const fintiInvitati = [];
 
-for (let i = 0; i < numeroFintaLista; i++) {
-	const randomNomiUser = Math.floor(Math.random * nomi.length);
-	const randomNomi = nomi[randomNomiUser];
+// const numeroFintiInvitati = parseInt(prompt("Quanti  finti invitati vuoi inserire"));
 
-	const randomCognomiUser = Math.floor(Math.random * cognomi.length);
-	const randomCognomi = cognomi[randomCognomiUser];
+// for (let i = 0; i < numeroFintiInvitati; i++) {
+// 	const randomNomiUser = Math.floor(Math.random * nomi.length);
+// 	const randomNomi = nomi[randomNomiUser];
 
-	const fintoInvitato = randomNomi + "" + randomCognomi;
+// 	const randomCognomiUser = Math.floor(Math.random * cognomi.length);
+// 	const randomCognomi = cognomi[randomCognomiUser];
 
-	fintalistaInvitati.push(fintoInvitato);
+// 	const fintoInvitato = randomNomi + "" + randomCognomi;
+
+// 	if (!fintiInvitati.includes(fintoInvitato)) {
+// 		fintiInvitati.push(fintoInvitato);
+// 	}
+// }
+// console.table(fintiInvitati);
+
+// // 3 SNAKE
+
+const number = [8, 4, 5, 72, 1, 2, 9, 87, 46];
+
+let som = 0;
+
+for (let i = 0; i < number.length; i++) {
+	const currentNumber = number[i];
+	console.log(currentNumber);
+
+	if (i % 2 != 0) {
+		som += currentNumber;
+	}
 }
 
-console.table(fintalistaInvitati);
+console.log("totale" + som);
